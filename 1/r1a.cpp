63f39b20b83d7ddf57fd6dc285daa9bea5e24e0d
Dominik Łempicki Kapitan <110813767+wedkarstwozkapitanem@users.noreply.github.com>
2024-12-10 18:48:04 +0100

    Złość

---
+++ 1/r1a.cpp
@@ -0,0 +1,13 @@
+/*
+    nazwa:Złość
+    autor:Dominik Łempicki Kapitan
+*/
+
+#include<iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(0),std::cin.tie(0);
+    long long n;std::cin >> n;
+    std::cout << n*60;
+    return EXIT_SUCCESS;
+}
