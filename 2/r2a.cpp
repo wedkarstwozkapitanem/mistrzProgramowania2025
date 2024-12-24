cc2feaa13af2396099efd706f20c2f103a340e4f
Dominik Łempicki Kapitan <110813767+wedkarstwozkapitanem@users.noreply.github.com>
2024-12-24 14:56:24 +0100

    Rozmowa kwalifikacyjna

---
+++ 2/r2a.cpp
@@ -0,0 +1,13 @@
+/*
+*    nazwa: Rozmowa kwalifikacyjna
+*    autor: Dominik Łempicki Kapitan
+*/
+
+#include<iostream>
+
+
+int main() {
+    long long a,b,p;std::cin >> a >> b >> p;
+    std::cout << (a*b==p ? "DOBRZE" : "TYLKO SZYBKO"); 
+    return EXIT_SUCCESS;
+}
