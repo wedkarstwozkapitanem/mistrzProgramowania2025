f2f88062e8f8f4213c2653eb6f1500b880fd27f8
Dominik Łempicki Kapitan <110813767+wedkarstwozkapitanem@users.noreply.github.com>
2024-12-24 14:55:58 +0100

    Siuuuuu

---
+++ 3/r3a.cpp
@@ -0,0 +1,14 @@
+/*
+*    nazwa: Siuuuuu
+*    autor: Dominik Łempicki Kapitan
+*/
+
+#include<iostream>
+
+int main() {
+    int n;std::cin >> n;
+    std::cout << "Si";
+    for(int i{};i<n;++i) std::cout << "u";
+
+    return EXIT_SUCCESS;
+}
