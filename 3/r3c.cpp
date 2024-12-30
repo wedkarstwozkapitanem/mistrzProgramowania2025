4212fd34ff0957216af05518096286db8f20f110
Dominik Łempicki Kapitan <110813767+wedkarstwozkapitanem@users.noreply.github.com>
2024-12-30 09:04:57 +0100

    Bajtocja Entertainment

--- 3/r3c.cpp
+++ 3/r3c.cpp
@@ -9,40 +9,11 @@
 #include <cmath>
 #include<queue>
 
-int main() {
-    std::ios_base::sync_with_stdio(0), std::cin.tie(0);
-    int n; 
-    std::cin >> n;
-    if (n < 1) { 
-        std::cout << "NIE";
-        return 0;
-    }
 
-    std::vector<long long> liczby(n);
-    long long suma = 0;
-
-
-    for (int i = 0; i < n; ++i) {
-        std::cin >> liczby[i];
-        suma += liczby[i];
-    }
-
-
-
+std::priority_queue<long long> dzielnik(const std::vector<long long> &liczby,const long long &suma){
     std::priority_queue<long long> dzielniki;
-   /* for (long long i = 1; i * i <= std::abs(suma); ++i) {
-        if (suma % i == 0) {
-            dzielniki.push_back(i);
-            dzielniki.push_back(-i);
-            if (i != suma / i) {
-                dzielniki.push_back(suma / i); 
-                dzielniki.push_back(-(suma/i));
-            }
-        }
-    }*/
-
-   long long sumaAk{};
-   for(const auto &i : liczby) {
+    long long sumaAk{};
+    for(const auto &i : liczby) {
         sumaAk += i;
         if(sumaAk != 0 && (suma%sumaAk)==0) {
             dzielniki.push(sumaAk);
@@ -55,14 +26,17 @@ int main() {
     dzielniki.push(suma);
     dzielniki.push(-suma);
     dzielniki.push(0);
+    return dzielniki;
+}
 
 
+bool dzielNaFragmenty(const std::vector<long long> &liczby,const long long suma,const long long n) {
+    auto dzielniki = dzielnik(liczby,suma);
     while(!dzielniki.empty()) {
         const long long aktualny = dzielniki.top();
         dzielniki.pop();
         long long sum = 0;
         std::vector<int> wynik;
-
 
         for (int i = 0; i < n; ++i) {
             sum += liczby[i];
@@ -71,15 +45,46 @@ int main() {
                 sum = 0; 
             }
         }
-
+        
         if (sum == 0 && wynik.size() >= 2 && wynik[wynik.size()-1]==n) {
             std::cout << "Tak\n" << wynik.size() << '\n';
             for (int i = 0; i < wynik.size(); ++i) std::cout << wynik[i] << ' ';
-            return 0;
+            return true;
         }
     }
+    return false;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(0), std::cin.tie(0);
+    int n; 
+    std::cin >> n;
+    if (n < 1) { 
+        std::cout << "NIE\n";
+        return 0;
+    }
+
+    std::vector<long long> liczby(n);
+    long long suma = 0;
 
 
-    std::cout << "Nie";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> liczby[i];
+        suma += liczby[i];
+    }
+
+    if(!dzielNaFragmenty(liczby,suma,n)) std::cout << "Nie\n";
     return 0;
 }
+
+
+   /* for (long long i = 1; i * i <= std::abs(suma); ++i) {
+        if (suma % i == 0) {
+            dzielniki.push_back(i);
+            dzielniki.push_back(-i);
+            if (i != suma / i) {
+                dzielniki.push_back(suma / i); 
+                dzielniki.push_back(-(suma/i));
+            }
+        }
+    }*/
